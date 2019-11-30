25cd24a1ccb9f9c05bbc52e98b68fd1ab2b3311c
HuynhDucHien <hien.huynh@deliveree.com>
2019-11-30 17:46:24 +0700

    update

--- shareplan/src/App.js
+++ shareplan/src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Dashboard from './components/dashboard/Dashboard';
-import ProjectDetails from './components/projects/ProjectDetails';
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+import SignUP from './components/auth/SignUp'
+import CreateProject from './components/projects/CreateProject'
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard} />
           <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin/" component={SignIn} />
+          <Route path="/signup/" component={SignUP} />
+          <Route path="/create/" component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
