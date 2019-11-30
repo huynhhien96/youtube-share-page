1e492c2e091f3590759352c0a16380cdbee7bbe4
HuynhDucHien <hien.huynh@deliveree.com>
2019-11-30 13:43:27 +0700

    create project

--- shareplan/src/components/projects/ProjectList.js
+++ shareplan/src/components/projects/ProjectList.js
@@ -1,27 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Video Title</span>
-                    <p>Posted by me</p>
-                    <p className="grey-text">Nov 30, 2019</p>
-                </div>
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Video Title</span>
-                    <p>Posted by me</p>
-                    <p className="grey-text">Nov 30, 2019</p>
-                </div>
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Video Title</span>
-                    <p>Posted by me</p>
-                    <p className="grey-text">Nov 30, 2019</p>
-                </div>
-
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     )
 }
---
+++ shareplan/src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return (
+        <div className="section card-content grey-text text-darken-3">
+            <h4 className="card-title">Video Title</h4>
+            <p>Posted by me</p>
+            <p className="grey-text">Nov 30, 2019</p>
+        </div>
+    )
+}
+
+export default ProjectSummary;
