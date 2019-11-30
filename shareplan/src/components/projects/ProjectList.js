aae7405d75beb360b96710f79c6b164401ad266f
HuynhDucHien <hien.huynh@deliveree.com>
2019-11-30 12:14:52 +0700

    Update dashboard

---
+++ shareplan/src/components/projects/ProjectList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summary">
+
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Video Title</span>
+                    <p>Posted by me</p>
+                    <p className="grey-text">Nov 30, 2019</p>
+                </div>
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Video Title</span>
+                    <p>Posted by me</p>
+                    <p className="grey-text">Nov 30, 2019</p>
+                </div>
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Video Title</span>
+                    <p>Posted by me</p>
+                    <p className="grey-text">Nov 30, 2019</p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList;
