e421c54f3798af01159f85b3a99dbde71d6f7c70
HuynhDucHien <hien.huynh@deliveree.com>
2019-12-01 14:49:29 +0700

    add thunk

--- shareplan/src/store/reducers/projectReducer.js
+++ shareplan/src/store/reducers/projectReducer.js
@@ -7,6 +7,10 @@ const iniState = {
 }
 
 const projectReducer = (state = iniState, action) => {
+    switch(action.type) {
+        case 'CREATE_PROJECT':
+            console.log('create project', action.project)
+    }
     return state
 }
 
