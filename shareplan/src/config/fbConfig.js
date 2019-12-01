7fec72c1a44626c012bd0a9619cc79ef8728b300
HuynhDucHien <hien.huynh@deliveree.com>
2019-12-01 17:21:47 +0700

    async data in Firebase

---
+++ shareplan/src/config/fbConfig.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+// change firebaseConfig to config
+var config = {
+    apiKey: "AIzaSyB5wX6_XUhPP13KLgEBVHCF-8uf_VR_4WU",
+    authDomain: "share-plan-hien.firebaseapp.com",
+    databaseURL: "https://share-plan-hien.firebaseio.com",
+    projectId: "share-plan-hien",
+    storageBucket: "share-plan-hien.appspot.com",
+    messagingSenderId: "667900380544",
+    appId: "1:667900380544:web:14441db7b9b5039bc76d65",
+    measurementId: "G-1NRY8KBMCS"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
+// firebase.analytics();
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
--- shareplan/src/store/reducers/projectReducer.js
+++ shareplan/src/store/reducers/projectReducer.js
@@ -9,7 +9,13 @@ const iniState = {
 const projectReducer = (state = iniState, action) => {
     switch(action.type) {
         case 'CREATE_PROJECT':
-            console.log('create project', action.project)
+            console.log('create project', action.project);
+            return state;
+        case 'CREATE_PROJECT_ERROR':
+            console.log('create project error', action.err);
+            return state;
+        default:
+            return state;
     }
     return state
 }
