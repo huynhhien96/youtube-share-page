9e911bcc64f33aff23588e4ccf29032a16b61c54
HuynhDucHien <hien.huynh@deliveree.com>
2019-12-01 12:04:29 +0700

    async code with redux

--- shareplan/src/components/projects/ProjectSummary.js
+++ shareplan/src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
         <div className="section card-content grey-text text-darken-3">
-            <h4 className="card-title">Video Title</h4>
-            <p>Posted by me</p>
+            <h4 className="card-title">{project.title}</h4>
+            <p>{project.content}</p>
             <p className="grey-text">Nov 30, 2019</p>
         </div>
     )
--- shareplan/src/store/reducers/projectReducer.js
+++ shareplan/src/store/reducers/projectReducer.js
@@ -1,7 +1,13 @@
-const iniState = {}
+const iniState = {
+    projects: [
+        { id: '1', title: 'Help me find peach', content: 'blah blah'},
+        { id: '2', title: 'Help me find peach 2', content: 'blah blah' },
+        { id: '3', title: 'Help me find peach 3', content: 'blah blah' }
+    ]
+}
 
 const projectReducer = (state = iniState, action) => {
     return state
 }
 
-export default projectReducer;
+export default projectReducer
