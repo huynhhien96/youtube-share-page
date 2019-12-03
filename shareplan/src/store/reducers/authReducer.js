7c54253ff8ed0471d507c104b761376daa612398
Hien Huynh <huynhhien.15596@gmail.com>
2019-12-03 18:29:36 +0700

    sign in

--- shareplan/src/store/reducers/authReducer.js
+++ shareplan/src/store/reducers/authReducer.js
@@ -1,7 +1,27 @@
-const iniState = {}
+const iniState = {
+    authError: null
+}
 
 const authReducer = (state = iniState, action) => {
-    return state
+    switch(action.type) {
+        case 'LOGIN_ERROR':
+            console.log('Login Error');
+            return {
+                ...state,
+                authError: 'Login Failed'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log('Login Success');
+            return {
+                ...state,
+                authError: null
+            }
+        case 'SIGNOUT_SUCCESS':
+            console.log('signout success');
+            return state;
+        default:
+            return state;
+    }
 }
 
 export default authReducer;
